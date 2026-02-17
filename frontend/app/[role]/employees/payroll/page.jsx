1d05831f7bbb8cb1e7d7864b524a08f160de020b
clicmaster4285 <software.clickmasters@gmail.com>
2026-02-17 18:05:28 +0500

    updating the model of emplyeor managemt

---
+++ frontend/app/[role]/employees/payroll/page.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import PayrollIntegration from '@/components/shared-components/employees/payroll/PayrollIntegration';
+
+const page = () => {
+  return (
+    <PayrollIntegration />
+  );
+};
+
+export default page;
---
+++ frontend/app/[role]/employees/performance/page.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import PerformanceManagement from '@/components/shared-components/employees/performance/PerformanceManagement';
+
+const page = () => {
+  return (
+    <PerformanceManagement />
+  );
+};
+
+export default page;
