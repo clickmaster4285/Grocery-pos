aac70deadc59f5c8b61000dffd50fb8af224c949
clicmaster4285 <software.clickmasters@gmail.com>
2026-02-19 09:23:05 +0500

    make a new page

---
+++ frontend/app/[role]/pos/receipts/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SalesHistory from '../sales/history/page';
+
+const ReceiptsPage = () => {
+  return (
+    <SalesHistory 
+      title="Receipt Management" 
+      description="Track, reprint, and manage all transaction receipts." 
+    />
+  );
+};
+
+export default ReceiptsPage;
